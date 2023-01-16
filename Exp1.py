237271860cb8520d79a1f83146ddb77b18045e67
AIshivam <115785850+AIshivam@users.noreply.github.com>
2023-01-16 16:42:04 +0530

    Add files via upload

---
+++ Exp1.py
@@ -0,0 +1,10 @@
+x = int (input ( "Distance between Two Cities in KM : "))
+
+meter = x*1000
+print(meter)
+feet = x*3280.84
+print(feet)
+inches = x*39370.1
+print(inches)
+centimeter = x*10000
+print(centimeter)2
