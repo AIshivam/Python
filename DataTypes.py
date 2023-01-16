3ef6551e3d714790f69d912de1337906723dc954
AIshivam <115785850+AIshivam@users.noreply.github.com>
2023-01-16 16:39:33 +0530

    Create DataTypes.py

---
+++ DataTypes.py
@@ -0,0 +1,12 @@
+
+print(2)
+print("hello india") #string
+print(12) #int
+print(101.1) #float
+print(1j) #complex
+print (2 ,5) #range
+x = dict(name = "shivam" , Age =18 ) #Dict
+print(x)
+print(("apple", "banana", "cherry")) #list
+y = tuple (("apple", "banana", "cherry")) #tuple
+print(y)
